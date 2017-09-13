3c5f8e71cdf1972d07e27e346a0b332bff2714a5
Mitch@mnemosyne <mitch.miller08@gmail.com>
2017-09-12 17:31:40 -0700

    added log file list and sentiment analyzer

--- analyzechat.py
+++ analyzechat.py
@@ -1,10 +1,23 @@
 import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
+import os import listdir
+from os.path import join
+
+## Build a list of logfiles saved locally
+def getLogList():
+    loglist = {}
+
+    for streamer in os.listdir('logs'):
+        streamerpath = join('logs',streamer)
+        loglist[streamer] = [join(streamerpath,log) for log in os.listdir(streamerpath)]
+
+    return loglist
 
 ## Load the log text into a list of individual messages
 def loadLog(filepath):
     logfile = open(filepath)
     messagelist = []
-    for line in file.readlines():
+    for line in logfile.readlines():
         messagelist.append(line.strip())
 
     return messagelist
@@ -13,7 +26,7 @@ def loadLog(filepath):
 ## a raw logfile line
 def separateMessage(rawmessage):
     timestampend = rawmessage.index(']') + 1
-    timestamp = rawmessage(:timestampend)
+    timestamp = rawmessage[:timestampend]
 
     userend = rawmessage[timestampend:].index(':') + timestampend + 1
     user = rawmessage[timestampend+1:userend-1]
@@ -22,12 +35,28 @@ def separateMessage(rawmessage):
 
     return timestamp, user, message
 
+##  Calculates the sentiment scores of 'message
+##  Returns a dictionary with keys 'pos', 'neu',
+##  'neg', and 'compunt'
+def analyzeMessage(message):
+    sia = SIA()
+    scores = sia.polarity_scores(message)
 
+    return scores
+
+def main():
+    logList = getLogList()
+    for streamer in list(logList.keys()):
+        ## DO SOMETHING WITH logList[streamer] (list of log file paths)
+
+if __name__ == '__main__':
+    main()
 
 ## NOTES:   from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
 ##          sia = SIA()
 ##          score = sia.polarity_scores(SOMETEXT)
 ##          score gives neg, neu, pos, and compound score
+##          avgpos = sum(score['pos'] for score in scoreslist) / len(scoreslist
 
 ## TODO:    Figure out how to average scores
 ##          Get user / subscriber count
