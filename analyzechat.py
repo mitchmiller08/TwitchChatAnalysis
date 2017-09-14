5a445e4badcf6e8250780f7c4c73c655cbd21847
Mitch@mnemosyne <mitch.miller08@gmail.com>
2017-09-13 17:59:11 -0700

    need to sort log files and run for all streamers

--- analyzechat.py
+++ analyzechat.py
@@ -1,15 +1,16 @@
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
-import os import listdir
-from os.path import join
+from os import listdir
+from os.path import join, basename
+import numpy as np
 
 ## Build a list of logfiles saved locally
 def getLogList():
     loglist = {}
 
-    for streamer in os.listdir('logs'):
+    for streamer in listdir('logs'):
         streamerpath = join('logs',streamer)
-        loglist[streamer] = [join(streamerpath,log) for log in os.listdir(streamerpath)]
+        loglist[streamer] = [join(streamerpath,log) for log in listdir(streamerpath)]
 
     return loglist
 
@@ -35,19 +36,75 @@ def separateMessage(rawmessage):
 
     return timestamp, user, message
 
-##  Calculates the sentiment scores of 'message
-##  Returns a dictionary with keys 'pos', 'neu',
-##  'neg', and 'compunt'
+## Split all messages in a given logfile
+def separateMessages(rawmessagelist):
+    timestamplist, usernamelist, messagelist = [],[],[]
+    for rawmessage in rawmessagelist:
+        timestamp, username, message = separateMessage(rawmessage)
+        timestamplist.append(timestamp)
+        usernamelist.append(username)
+        messagelist.append(message)
+
+    return timestamplist, usernamelist, messagelist
+
+## Calculates the sentiment scores of 'message
+## Returns a dictionary with keys 'pos', 'neu',
+## 'neg', and 'compunt'
 def analyzeMessage(message):
     sia = SIA()
     scores = sia.polarity_scores(message)
 
     return scores
 
+## Do sentiment analysis on all messages in a list 
+## Return the average and standard deviation of scores
+## for negative, positive, neutral, and compound
+def analyzeMessages(messagelist):
+    poslist,neglist,neulist,compoundlist = [],[],[],[]
+    for message in messagelist:
+        scores = analyzeMessage(message)
+        poslist.append(scores['pos'])
+        neglist.append(scores['neg'])
+        neulist.append(scores['neu'])
+        compoundlist.append(scores['compound'])
+
+    negativeresults = [np.mean(neglist),np.std(neglist)]
+    positiveresults = [np.mean(poslist),np.std(poslist)]
+    neutralresults = [np.mean(neulist),np.std(neulist)]
+    compoundresults = [np.mean(compoundlist),np.std(compoundlist)]
+
+    return negativeresults, neutralresults, positiveresults, compoundresults
+
+## Format scores for output to file
+def formatOutput(logpath,negres,neures,posres,compres):
+    output = basename(logpath)
+    output += '\t' + str(negres[0]) + ' ' + str(negres[1]) + '\t'
+    output += '\t' + str(neures[0]) + ' ' + str(neures[1]) + '\t'
+    output += '\t' + str(posres[0]) + ' ' + str(posres[1]) + '\t'
+    output += '\t' + str(compres[0]) + ' ' + str(compres[1]) + '\n'
+    return output
+
 def main():
     logList = getLogList()
-    for streamer in list(logList.keys()):
-        ## DO SOMETHING WITH logList[streamer] (list of log file paths)
+
+    #for streamer in logList.keys():
+    # temporary for testing - begin
+    list = ['Day9tv']
+    for streamer in list:
+    # temporary for testing - end
+        print(streamer)
+        scoresfile = open(join('scores',streamer),'w')
+        logpaths = logList[streamer]
+
+        for logpath in logpaths:
+                print(basename(logpath))
+                rawmessagelist = loadLog(logpath)
+                timestamplist, usernamelist, messagelist = separateMessages(rawmessagelist)
+
+                negativeresults, neutralresults, positiveresults, compoundresults = analyzeMessages(messagelist)
+                scoresfile.write(formatOutput(logpath,negativeresults,neutralresults,positiveresults,compoundresults))
+
+        scoresfile.close()
 
 if __name__ == '__main__':
     main()
@@ -58,7 +115,6 @@ if __name__ == '__main__':
 ##          score gives neg, neu, pos, and compound score
 ##          avgpos = sum(score['pos'] for score in scoreslist) / len(scoreslist
 
-## TODO:    Figure out how to average scores
+## TODO:    Sort log files before analysis 
 ##          Get user / subscriber count
 ##          Calculate message frequency from timestamps
-##          Do analysis
